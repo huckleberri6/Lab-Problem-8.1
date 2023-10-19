232a68272cce71358883314a4ef4c8f76eed5260
huckleberri6 <j-devore.1@onu.edu>
2023-10-19 11:04:16 -0400

    Completed countVowel function.

--- Lab-Problem-8.1/solution.cpp
+++ Lab-Problem-8.1/solution.cpp
@@ -3,16 +3,31 @@
 #include <string>
 using namespace std;
 
-int countCharacter(string str);
+int countVowel(string str);
 
 int main()
 {
-	string s = "hello";
-	int l = countCharacter(s);
+	string s = "HELLO";
+	int l = countVowel(s);
 	cout << l;
 }
 
-int countCharacter(string str)
+int countVowel(string str)
 {
-	return str.length();
+	int vowels = 0;
+	for (int i = 0; i < str.length(); i++)
+	{
+		if (str.substr(i, 1) == "a" || str.substr(i, 1) == "A")
+			vowels++;
+		else if (str.substr(i, 1) == "e" || str.substr(i, 1) == "E")
+			vowels++;
+		else if (str.substr(i, 1) == "i" || str.substr(i, 1) == "I")
+			vowels++;
+		else if (str.substr(i, 1) == "o" || str.substr(i, 1) == "O")
+			vowels++;
+		else if (str.substr(i, 1) == "u" || str.substr(i, 1) == "U")
+			vowels++;
+	}
+
+	return vowels;
 }
