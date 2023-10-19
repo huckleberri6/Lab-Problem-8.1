844bdf6bbdd96bf5c6effbb82eb8a852d69ede23
huckleberri6 <j-devore.1@onu.edu>
2023-10-19 10:55:30 -0400

    Completed countCharacter function.

--- Lab-Problem-8.1/solution.cpp
+++ Lab-Problem-8.1/solution.cpp
@@ -1,1 +1,18 @@
 
+#include <iostream>
+#include <string>
+using namespace std;
+
+int countCharacter(string str);
+
+int main()
+{
+	string s = "hello";
+	int l = countCharacter(s);
+	cout << l;
+}
+
+int countCharacter(string str)
+{
+	return str.length();
+}
